57f20c2a89d289e0a3d08a76d3e9ef855a98ed09
Andrew Pham <andrewbapham@outlook.com>
2024-12-12 18:14:11 -0500

    update for consistent naming

--- day4/day4part2.go
+++ day4/day4part2.go
@@ -7,7 +7,7 @@ var (
 	d_l_dir = Direction{X: -1, Y: 1}
 )
 
-func is_target_letter(s string, target []string) bool {
+func isTargetLetter(s string, target []string) bool {
 	for _, t := range target {
 		if s == t {
 			return true
@@ -16,7 +16,7 @@ func is_target_letter(s string, target []string) bool {
 	return false
 }
 
-func check_corners(grid []string, pos Direction) bool {
+func checkCorners(grid []string, pos Direction) bool {
   if pos.X < 1 || pos.X >= len(grid[0]) - 1 || pos.Y < 1 || pos.Y >= len(grid) - 1{
     return false
   }
@@ -27,7 +27,7 @@ func check_corners(grid []string, pos Direction) bool {
 	bottom_right := string(grid[pos.Y+1][pos.X+1])
 
 	target := []string{"M", "S"}
-	if !is_target_letter(top_left, target) || !is_target_letter(top_right, target) || !is_target_letter(bottom_left, target) || !is_target_letter(bottom_right, target) {
+	if !isTargetLetter(top_left, target) || !isTargetLetter(top_right, target) || !isTargetLetter(bottom_left, target) || !isTargetLetter(bottom_right, target) {
 		return false
 	}
   if top_left == bottom_right || top_right == bottom_left {
@@ -42,7 +42,7 @@ func part2() {
 	for i := 0; i < len(lines); i++ {
 		for j := 0; j < len(lines[i]); j++ {
       if string(lines[i][j]) == "A"{
-        if check_corners(lines, Direction{X: j, Y: i}){
+        if checkCorners(lines, Direction{X: j, Y: i}){
           count++
         }
       }
